d7f3f4ddd6e7022c7165bd0b8f221e27aa2126de
alberth <alberth.g7dev@gmail.com>
2025-05-31 12:10:04 -0400

    Updates

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p>© 2025 Ayni Moda – <span style={{fontStyle:'italic'}}>“Tejiendo futuro con raíces”</span></p>
-    </footer>
-  );
-}
